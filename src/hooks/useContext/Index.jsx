b0b8a01b21404159424312ce3c9ed7aed76b9af8
abriilo <abrilojson@gmail.com>
2022-07-28 17:06:34 +0300

    useContext hook added

--- src/hooks/useContext/Index.jsx
+++ src/hooks/useContext/Index.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState, createContext, useContext} from 'react';
 
+// creating theme context
+let ThemeContext = createContext();
 
-const Index = () =>{
+const Context = () => {
+    const [theme, setTheme] = useState('light');
+
+    return(
+       <ThemeContext value={theme}>
+        <Component />
+       </ThemeContext>
+    )
+}
+const Component = () =>{
+    let theme = useContext(ThemeContext);
 
     return(
         <>
-
+        <h1>Theme Mode: {theme}</h1>
         </>
     )
-};
-
-export default Index;
+}
+export default Context;
