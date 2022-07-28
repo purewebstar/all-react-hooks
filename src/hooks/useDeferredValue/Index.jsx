16c727fce591cfd2a61a2223cd188c219a353438
abriilo <abrilojson@gmail.com>
2022-07-28 17:10:22 +0300

    useDeferredValue hook added

--- src/hooks/useDeferredValue/Index.jsx
+++ src/hooks/useDeferredValue/Index.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import { useDeferredValue, useState } from 'react';
 
+const SlowUI = () => (
+  <>
+    {Array(50)
+      .fill(1)
+      .map((_, index) => (
+        <span key={index}>{100000} </span>
+      ))}
+  </>
+);
 
-const Index = () =>{
+const UseDeferredValue = () => {
+  const [value, setValue] = useState(0);
+  const deferredValue = useDeferredValue(value);
 
-    return(
-        <>
+  const handleClick = () => {
+    setValue(value + 1);
+  };
 
-        </>
-    )
-};
+  return (
+    <>
+      <button onClick={handleClick}>{value}</button>
+      <div>DeferredValue: {deferredValue}</div>
+      <div>
+        <SlowUI />
+      </div>
+    </>
+  );
+}
 
-export default Index;
+export default UseDeferredValue;
