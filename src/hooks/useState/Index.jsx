cbd4ee3ddbf9bfd4b53850d25740e2598f60dcfe
abriilo <abrilojson@gmail.com>
2022-07-28 16:56:08 +0300

    usestate hook added

--- src/hooks/useState/Index.jsx
+++ src/hooks/useState/Index.jsx
@@ -1,13 +1,28 @@
 import React, {useState} from 'react';
 
 const Index = () =>{
-            
+    // Always initialize a state with:                                                        
+    // null,undefined,empty array,string,num,...                                       
+    const [num, setNum] = useState(0);
+    const [data, setData] = useState([1,8,9,3,5]);                                         
+    /**
+     * Update states in: Functions, Hooks, ...                                                 
+     */
+      // setNum(10);                     
+      // setData([1,5,9,0,3])             
     return(
-        <>
-         <h1></h1>                         
-        
-        </>
-    )
+    <>
+    <h1>Num is: {num}</h1>                                            
+             
+    {  
+    data&&data.map((d,i)=>(                     
+        <React.Fragment key={i}  >                                              
+        <h3>{d&&d}</h3>                            
+        </React.Fragment>                   
+    ))                 
+    }
+    </>
+    );
 };
 
 export default Index;
