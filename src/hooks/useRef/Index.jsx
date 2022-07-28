d8a9516ffc6839e0c8d8ef890b4d2b90b0196de8
abriilo <abrilojson@gmail.com>
2022-07-28 16:57:41 +0300

    useRef hook added

--- src/hooks/useRef/Index.jsx
+++ src/hooks/useRef/Index.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-
-
-const Index = () =>{
-
+import React, {useState, useRef, useEffect} from 'react';      
+            
+//         
+const UseRef = () =>{
+    const[value, setValue] = useState('');
+    const count = useRef(0);
+    
+    useEffect(() => {
+        count.current = count.current + 1;
+    });
     return(
-        <>
-
-        </>
+      <>
+      <h1>Rendering: {count.current} times</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      </>
     )
 };
 
-export default Index;
+export default UseRef;
