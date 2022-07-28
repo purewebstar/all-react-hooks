01df3c3223b6f02bfa5be0785c36e46f1b262232
abriilo <abrilojson@gmail.com>
2022-07-28 17:05:55 +0300

    custom hook added

--- src/hooks/customHook/Index.jsx
+++ src/hooks/customHook/Index.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';         
+// custom hook           
+const useFetch = (url) =>{
+ const[data, setData] = useState(null);           
 
+ useEffect(() =>{
+   fetch(url)
+   .then((res)=> res.json())
+   .then((data)=> setData(data));
+ },[url]);
 
-const Index = () =>{
-
+ return [data];
+}
+const CustomHook = () =>{   
+    const [data] = useFetch('your_url');
+    
     return(
-        <>
-
-        </>
-    )
+      <>
+      {data&&data.map((d, i) => {          
+        return <p key={i}>{d}</p>
+      })
+      }                      
+      </>
+    );
 };
 
-export default Index;
+export default CustomHook;
