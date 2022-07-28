66be957b8a5e217ddfc19323124f9395caea2ff2
abriilo <abrilojson@gmail.com>
2022-07-28 17:12:25 +0300

    useTransition hook added

--- src/hooks/useTransition/Index.jsx
+++ src/hooks/useTransition/Index.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import { useEffect, useState, useTransition } from 'react';
 
+const SlowUI = ({ value }) => (
+  <>
+    {Array(value)
+      .fill(1)
+      .map((_, index) => (
+        <span key={index}>{value - index} </span>
+      ))}
+  </>
+);
 
-const Index = () =>{
+const UseTransition = () => {
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(100000);
+  const [isPending, startTransition] = useTransition();
 
-    return(
-        <>
+  const handleClick = () => {
+    setValue(value + 1);
+    startTransition(() => setValue2(value2 + 1));
+  };
 
-        </>
-    )
-};
+  return (
+    <>
+      <button onClick={handleClick}>{value}</button>
+      <div
+        style={{
+          opacity: isPending ? 0.5 : 1,
+        }}
+      >
+        <SlowUI value={value2} />
+      </div>
+    </>
+  );
+}
 
-export default Index;
+export default UseTransition;
