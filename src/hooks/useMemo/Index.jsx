2ca1506c32b59275e5bc58fb80fba8a0ac6953b6
abriilo <abrilojson@gmail.com>
2022-07-28 17:03:43 +0300

    useMemo hook added

--- src/hooks/useMemo/Index.jsx
+++ src/hooks/useMemo/Index.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-
-
-const Index = () =>{
-
+import React, {useState, useMemo} from 'react';      
+// expensive computation eg.              
+const expensive = (arg) =>{    
+   for(let i=0; i<100000000; i++){         
+     arg+=1;
+   }
+   return arg;
+}               
+//         
+const UseMemo = () =>{
+    const[count, setCount] = useState(0);
+    const [data, setData] = useState([]);
+    // returns memorized value & updates only when count changed            
+    const add = useMemo(() => expensive((count), [count]));          
+    // count handler
+    const handleCount = () =>{
+        setCount((c) => c+1);
+    }      
+    const handleData = () =>{
+        setData((d) => [...d, 'something'])
+    }
+    console.log(`add: ${add}`, data)
     return(
-        <>
-
-        </>
+      <>
+      <p>Count: {count}</p>               
+      <button onClick={handleCount} >                                 
+        Add Count             
+      </button>
+      <br/>
+      <button onClick={handleData} >                                 
+        Add Data             
+      </button>
+      </>
     )
 };
 
-export default Index;
+export default UseMemo;
