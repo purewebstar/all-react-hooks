55dff8225d43854df0860e0e36cea0c41aa29970
abriilo <abrilojson@gmail.com>
2022-07-28 17:08:08 +0300

    useReducer hook added

--- src/hooks/useReducer/Index.jsx
+++ src/hooks/useReducer/Index.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useEffect, useReducer} from 'react';      
+// initial count    
+let initialState = 0;                  
+// reducer function               
+const reducer = (state = initialState, action)=>{
+    // check action type                
+    switch (action.type){
+        case 'UPDATE':
+        return {
+            ...state,
+            count: (action.payload)
+        };
+        default: 
+        return state;
+    };
+};
+// reducer
+const UseReducer = () =>{
+    const[num, dispatch] = useReducer(reducer, initialState);
 
-
-const Index = () =>{
-
+    const handleCount = () =>{
+        dispatch({type: 'UPDATE', payload: 1})
+    }
+   // console.log(num&&num.count)            
     return(
-        <>
-
-        </>
+      <>
+      <p>Count: {num&&num.count}</p>               
+      <button onClick={handleCount} >                                
+        Add Count             
+      </button>
+      </>
     )
 };
 
-export default Index;
+export default UseReducer;
